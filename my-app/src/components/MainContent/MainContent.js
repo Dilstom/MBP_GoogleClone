8fac7d633d4caf23f9023f7d82162e95be9ab561
Dil Sim <dilyasom@gmail.com>
2018-06-03 09:53:28 -0700

    search functionality

--- my-app/src/components/MainContent/MainContent.js
+++ my-app/src/components/MainContent/MainContent.js
@@ -2,10 +2,46 @@ import React, { Component } from 'react';
 import './index.css';
 
 class MainContent extends Component {
+    state = {
+        searchCriteria: '',
+    }
+
+    handleChange = e => {
+        // console.log(e.target.value);
+        this.setState({ searchCriteria: e.target.value })
+    }
+
+    handleSubmit = () => {
+        // console.log('clicked')
+        // https://www.google.com/search?q=stuff
+        // let inputSearch = document.getElementsByTagName('input')[0].value;
+        // console.log(this.state.searchCriteria);
+        let search = this.state.searchCriteria;
+        // let combined = 'https://www.google.com/search?q=' + search;
+        let combined = `https://www.google.com/search?q=${search}`
+        // console.log(combined);
+        window.location.href = combined; // navigate to combined address
+    }
+
+    handleFeeling = () => {
+        window.location.href = `https://www.${this.state.searchCriteria}.com`
+    }
     render() {
         return (
             <div className='mainContent_wrapper'>
-                MainContent
+                <div className='inputField'>
+                    <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png' alt='Google Logo' />
+                    <input
+                        type='text'
+                        onChange={this.handleChange}
+                    />
+                </div>
+                <div className='search_buttons'>
+                    <button
+                        onClick={this.handleSubmit}
+                    >Google Search</button>
+                    <button onClick={this.handleFeeling}>I'm feeling lucky</button>
+                </div>
             </div>
         );
     }
